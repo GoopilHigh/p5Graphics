944d91b0a54ee78e9c38749f1245f81aab18cea1
lejeun_b <stephane.lejeune@epitech.eu>
2018-04-04 14:08:16 +0800

    Initialisation
    Random vs Perlin noise

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # p5Graphics
+
+To be able to run all of this you need to download the Processing Ide: https://processing.org/download/
+after add the "p5" library in the IDE take the code and push button Play.
---
+++ RandomPointInAxis/RandomPointInAxis.js
@@ -0,0 +1,18 @@
+var xoff1 = 0;
+var xoff2 = 10000;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(51);
+  
+  var x = random(width);
+  var y = random(height);
+  
+  xoff1 += 0.02;
+  xoff2 += 0.02;
+  
+  ellipse(x, y, 24, 24);
+}
